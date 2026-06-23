713feb21e0ec48a23b5272379f839a8fded1ab25
Adarsh Chaudhary <chaudharyadarsh9919@gmail.com>
2026-06-23 17:27:31 +0530

    force dynamic in / route in web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { prisma } from "@repo/db/prisma";
 
+export const dynamic = "force-dynamic";
+
 const page = async () => {
   const user = await prisma.user.findFirst();
   return (
